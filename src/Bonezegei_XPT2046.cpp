e26371ab3b1e57b037a409f222f1703bcfaec306
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2023-07-18 23:59:56 +0800

    Update Bonezegei_XPT2046.cpp

--- src/Bonezegei_XPT2046.cpp
+++ src/Bonezegei_XPT2046.cpp
@@ -13,6 +13,10 @@ Bonezegei_XPT2046::Bonezegei_XPT2046(uint8_t cs, uint8_t irq) {
   _irq = irq;
 }
 
+Bonezegei_XPT2046::Bonezegei_XPT2046(uint8_t cs) {
+  _cs = cs;
+}
+
 void Bonezegei_XPT2046::begin() {
   vspi = new SPIClass(VSPI);
   vspi->begin();
@@ -21,8 +25,8 @@ void Bonezegei_XPT2046::begin() {
   vspi->setBitOrder(MSBFIRST);
 
   pinMode(_cs, OUTPUT);
-  pinMode(_irq, INPUT);
-  attachInterrupt(digitalPinToInterrupt(_irq), irq_callback, FALLING);
+  /*   pinMode(_irq, INPUT);
+  attachInterrupt(digitalPinToInterrupt(_irq), irq_callback, FALLING); */
 }
 
 void Bonezegei_XPT2046::setCallBack(void (*cb)()) {
@@ -30,12 +34,21 @@ void Bonezegei_XPT2046::setCallBack(void (*cb)()) {
 }
 
 
-void Bonezegei_XPT2046::getInput() {
+uint8_t Bonezegei_XPT2046::getInput() {
 
   vspi->setFrequency(XPT2046_SPISPEED);
   digitalWrite(_cs, LOW);
   vspi->transfer(0xB1);
-  x = vspi->transfer16(0x91) >> 3;
-  y = vspi->transfer16(0xD1) >> 3;
-  z = vspi->transfer16(0xC1) >> 3;
+  int16_t z1 = vspi->transfer16(0xC1) >> 3;
+  int16_t z2 = vspi->transfer16(0x91) >> 3;
+  x = vspi->transfer16(0xD0) >> 3;
+  y = vspi->transfer16(0) >> 3;
+  digitalWrite(_cs, HIGH);
+  z = z1 + 4095 - z2;
+
+  if (z > 500) {
+    return 1;
+  } else {
+    return 0;
+  }
 }
